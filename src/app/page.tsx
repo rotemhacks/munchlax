c6c73545796e803b07d42e457cfbacffb7dbbdfa
Rotem S. <rotemdraws@gmail.com>
2025-01-07 16:38:10 +0200

    add sign in function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   const session = await auth();
 
   if (session?.user) {
-    void api.post.getLatest.prefetch();
+    // void api.post.getLatest.prefetch(); TODO: See how that works for prefetching entries
     redirect("/dashboard");
   }
   return (
